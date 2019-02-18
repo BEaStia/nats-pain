22e2183b50523c77f5d43297a3373965d042fff6
Igor Pavlov <gophan1992@gmail.com>
2019-02-18 11:33:11 +0300

    added options to nats pain initializer, connection_pools for nats and stan

--- lib/nats/pain/connection.rb
+++ lib/nats/pain/connection.rb
@@ -8,54 +8,62 @@ require 'connection_pool'
 module Nats
   module Pain
     class Connection
-      attr_reader :nats_client, :stan_client
+      attr_reader :nats_pool, :stan_pool
 
       extend Dry::Configurable
 
-      setting :pool, ENV.fetch('PAIN_POOL_SIZE') { 5 }, reader: true
-      setting :pool_timeout, ENV.fetch('PAIN_POOL_TIMEOUT') { 5 }, reader: true
+      def self.current
+        @current ||= Nats::Pain::Connection.new
+      end
+
+      class << self
+        attr_writer :current
+      end
+
+      setting :nats_enabled, ENV.fetch('PAIN_NATS_ENABLED') { true }, reader: true
+      setting :nats_pool_size, ENV.fetch('PAIN_NATS_POOL_SIZE') { 5 }, reader: true
+      setting :nats_pool_timeout, ENV.fetch('PAIN_NATS_POOL_TIMEOUT') { 5 }, reader: true
       setting :nats_service_name, ENV.fetch('NATS_SERVICE_NAME') { 'painful_service' }, reader: true
       setting :nats_servers, ENV.fetch('NATS_SERVERS_URLS') { 'nats://localhost:4222' }, reader: true
+
+      setting :stan_enabled, ENV.fetch('PAIN_STAN_ENABLED') { true }, reader: true
+      setting :stan_pool_size, ENV.fetch('PAIN_STAN_POOL_SIZE') { 5 }, reader: true
+      setting :stan_pool_timeout, ENV.fetch('PAIN_STAN_POOL_TIMEOUT') { 5 }, reader: true
       setting :stan_servers, ENV.fetch('STAN_SERVERS_URLS') { 'nats://localhost:4223' }, reader: true
       setting :stan_cluster_name, ENV.fetch('STAN_CLUSTER_NAME') { 'worki_cluster' }, reader: true
 
-      def self.current
-        pool_size = Nats::Pain::Connection.pool
-        pool_timeout = Nats::Pain::Connection.pool_timeout
-        @current ||= ConnectionPool.new(size: pool_size, timeout: pool_timeout) do
-          Nats::Pain::Connection.new
-        end
-      end
-
-      def self.current=(val)
-        pool_size = Nats::Pain::Connection.pool
-        pool_timeout = Nats::Pain::Connection.pool_timeout
-        @current = ConnectionPool.new(size: pool_size, timeout: pool_timeout) do
-          val
-        end
-      end
-
-      def initialize
+      def initialize(opts = {})
         @logger = Ougai::Logger.new(STDOUT)
-
-        @nats_client = NatsListener::Client.new(logger: @logger, skip: false, catch_errors: false)
-        @stan_client = NatsStreamingListener::StreamingClient.new(logger: @logger, skip: false, catch_errors: false)
-
-        establish_connections
+        load_nats(opts)
+        load_stan(opts)
       end
 
-      def establish_connections
-        @nats_client.establish_connection(
-          service_name: Connection.nats_service_name,
-          nats: { servers: Connection.nats_servers.split(',') }, # Options passed to nats connector
-        )
+      def load_nats(opts = {})
+        return unless opts.fetch(:nats_enabled) { Nats::Pain::Connection.nats_enabled }
 
-        @stan_client.establish_connection(
-          service_name: Connection.nats_service_name,
-          nats: { servers: Connection.stan_servers.split(',') }, # Options passed to nats connector
-          cluster_name: Connection.stan_cluster_name, # Cluster of nats-streaming that you're connecting to
-          client_id: SecureRandom.hex(13).to_s # Id of a client(nats-streaming works with unique client_id)
-        )
+        @nats_pool = ConnectionPool.new(size: Nats::Pain::Connection.nats_pool_size, timeout: Nats::Pain::Connection.nats_pool_timeout) do
+          connection = NatsListener::Client.new(logger: @logger, skip: false, catch_errors: false)
+          connection.establish_connection(
+            service_name: opts.fetch(:nats_service_name) { Connection.nats_service_name },
+            nats: { servers: opts.fetch(:nats_servers) { Connection.nats_servers }.split(',') }, # Options passed to nats connector
+          )
+          connection
+        end
+      end
+
+      def load_stan(opts = {})
+        return unless opts.fetch(:stan_enabled) { Nats::Pain::Connection.stan_enabled }
+
+        @stan_pool = ConnectionPool.new(size: Nats::Pain::Connection.stan_pool_size, timeout: Nats::Pain::Connection.stan_pool_timeout) do
+          connection = NatsStreamingListener::StreamingClient.new(logger: @logger, skip: false, catch_errors: false)
+          connection.establish_connection(
+            service_name: opts.fetch(:nats_service_name) { Connection.nats_service_name },
+            nats: { servers: opts.fetch(:stan_servers) { Connection.stan_servers }.split(',') }, # Options passed to nats connector
+            cluster_name: opts.fetch(:stan_cluster_name) { Connection.stan_cluster_name }, # Cluster of nats-streaming that you're connecting to
+            client_id: SecureRandom.hex(13).to_s # Id of a client(nats-streaming works with unique client_id)
+          )
+          connection
+        end
       end
     end
   end
--- spec/nats/pain/connection_spec.rb
+++ spec/nats/pain/connection_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Nats::Pain::Connection do
   describe '.new' do
-    subject { described_class.new }
+    let(:opts) { {} }
+    subject { described_class.new(opts) }
 
     it 'should create connection' do
       expect { subject }.not_to raise_exception
@@ -9,9 +10,23 @@ RSpec.describe Nats::Pain::Connection do
 
   describe '.current' do
     subject { described_class.current }
+    let(:topic) { 'topic' }
+    let(:message) { '{"a": "123"}' }
 
     it 'should return connection pool' do
-      expect(subject).to be_an_instance_of(ConnectionPool)
+      expect(subject).to be_an_instance_of(described_class)
+    end
+
+    it 'should publish nats event' do
+      expect_any_instance_of(NatsListener::Client).to receive(:publish).with(topic, message).and_return(true)
+      subject.nats_pool&.with { |conn| conn.publish(topic, message) }
+    end
+
+    describe 'with disabled nats' do
+      let(:opts) { { nats_enabled: false } }
+      it 'should not fail' do
+        expect { subject.nats_pool&.with { |conn| conn.publish(topic, message) } }.not_to raise_exception
+      end
     end
   end
 end
